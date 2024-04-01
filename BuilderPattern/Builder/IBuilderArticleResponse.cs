3ebbb283dd48df59aabdbdc4819df868da2037d0
Git-Lucas <lucas.oliver_2000@hotmail.com>
2024-03-31 21:27:42 -0300

    [BP] - feat: Adicionado padrão Builder à aplicação, para retornos customizados de um objeto de artigo

---
+++ BuilderPattern/Builder/IBuilderArticleResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace BuilderPattern.Builder;
+public interface IBuilderArticleResponse
+{
+    void BuildTitle();
+    void BuildIntroduction();
+    void BuildBody();
+    void BuildMethods();
+    void BuildConclusion();
+}
